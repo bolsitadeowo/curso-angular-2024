c7186591f3c105ee1103991e8b02ef88b077cf56
alexis marin <alephys@outlook.es>
2024-01-19 14:15:46 -0300

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { SlidebarComponent } from "./slidebar/slidebar.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, SlidebarComponent]
+})
+export class AppComponent {
+  title = 'MiprimerApp Angular';
+}
